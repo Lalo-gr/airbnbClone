67129ebcb73a180e16d002e9de38d19063ec10c8
Lalo-gr <einajera741@gmail.com>
2023-04-04 12:44:50 -0500

    "Mapping"

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,27 @@ import "./App.css";
 import NavBar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import data from "./data";
 
 function App() {
+  const cards = data.map((item) => {
+    return (
+      <Card
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+      />
+    );
+  });
+
   return (
     <div>
       <NavBar />
       <Hero />
-      <Card
-        img = "/src/assets/katie.png"
-        rating = "5.0"
-        reviewCount= {6}
-        country = "USA"
-        title = "Life Lessons with Katie"
-        price = {136}
-      />
+      {cards}
     </div>
   );
 }
